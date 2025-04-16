88cc316aa48fae8160b29ef24620560c278f8dec
akanksha089 <achaturvdi089@gmail.com>
2025-04-16 17:17:19 +0530

    savebackend cde

--- backend/config/mysql_database.js
+++ backend/config/mysql_database.js
@@ -1,33 +1,33 @@
-const mysql = require("mysql2/promise");
-const dotenv = require("dotenv");
-dotenv.config({ path: "backend/config/config.env" });
-
-const mysqlPool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "nodejs2",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
-module.exports = mysqlPool;
-
-
 // const mysql = require("mysql2/promise");
 // const dotenv = require("dotenv");
 // dotenv.config({ path: "backend/config/config.env" });
 
 // const mysqlPool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   port: process.env.DB_PORT,
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "nodejs2",
 //   waitForConnections: true,
 //   connectionLimit: 10,
 //   queueLimit: 0,
 // });
-
 // module.exports = mysqlPool;
+
+
+ const mysql = require("mysql2/promise");
+ const dotenv = require("dotenv");
+ dotenv.config({ path: "backend/config/config.env" });
+
+ const mysqlPool = mysql.createPool({
+   host: process.env.DB_HOST,
+   user: process.env.DB_USER,
+   password: process.env.DB_PASSWORD,
+   database: process.env.DB_NAME,
+   port: process.env.DB_PORT,
+   waitForConnections: true,
+   connectionLimit: 10,
+   queueLimit: 0,
+ });
+
+ module.exports = mysqlPool;
 
